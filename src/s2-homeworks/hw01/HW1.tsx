42bf60644eafc8349e07f5b9631a9d632dc5febe
allexmorozovv <allexmorozovv@gmail.com>
2022-11-28 14:33:56 +0300

    add homework 1

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -31,10 +31,10 @@ export const message0: MessageType = {
     id: 0,
     user: {
         avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        name: 'John',  // можно менять
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
+        text: "Hi! I'm John!!!", // можно менять
         time: '22:00', // можно менять
     },
 }
@@ -42,10 +42,10 @@ export const friendMessage0: MessageType = {
     id: 100,
     user: {
         avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
+        name: 'Alex', // можно менять
     },
     message: {
-        text: 'зеркальное сообщение для тренировки css', // можно менять
+        text: "Hi! I'm Alex!!!", // можно менять
         time: '22:00', // можно менять
     },
 }
