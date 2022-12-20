2505dcd2377a5eea4d41be3d9d28b2e515cde77b
allexmorozovv <allexmorozovv@gmail.com>
2022-12-20 06:12:32 +0300

    fix homework 5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -32,7 +32,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
                         className={({isActive}) =>
-                            (isActive ? s.active : ' ')}
+                            (isActive ? s.active : '')}
                     >
                         Pre-junior
                     </NavLink>
@@ -41,7 +41,8 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.JUNIOR}
                         onClick={handleClose}
                         className={({isActive}) =>
-                            (isActive ? s.active : ' ')}
+                            (isActive ? s.active : '')}
+                        end
                     >
                         Junior
                     </NavLink>
@@ -50,7 +51,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
                         className={({isActive}) =>
-                            (isActive ? s.active : ' ')}
+                            (isActive ? s.active : '')}
                     >
                         Junior Plus
                     </NavLink>
